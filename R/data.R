f581793bd79df5cc7bf6465f8a92d538a3dcc630
Rithwik <0rithwik@gmail.com>
2016-02-23 14:30:42 +0530

    sample data: wbcd

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Wisconsin Breast Cancer Data
+#'
+#' @format A data frame with 699 rows and 11 variables:
+#' \describe{
+#'   \item{Sample.code.number}
+#'   \item{Clump.Thickness}
+#'   \item{Uniformity.of.Cell.Size}
+#'   \item{Uniformity.of.Cell.Shape}
+#'   \item{Marginal.Adhesion}
+#'   \item{Single.Epithelial.Cell.Size}
+#'   \item{Bare.Nuclei}
+#'   \item{Bland.Chromatin}
+#'   \item{Normal.Nucleoli}
+#'   \item{Mitoses}
+#'   \item{Class}
+#' }
+#' @source \url{}
+"wbcd"
