64572be4eab6fd28848442b639e412ad89b2c51f
Rithwik <0rithwik@gmail.com>
2016-02-23 11:30:03 +0530

    the game's afoot

---
+++ R/bnet.R
@@ -0,0 +1,84 @@
+#' Preprocess
+#'
+#' @param df Dataframe to be processed
+#' @export
+#' @importFrom infotheo discretize
+preprocess <- function(df) {
+  ids <- colnames(df)
+  df[, sapply(df, is.character)] <- Map(as.factor, df[, sapply(df, is.character)])
+  df[, sapply(df, is.numeric)] <- data.frame(
+    lapply(df[, sapply(df, is.numeric)], infotheo::discretize)
+  )
+  df[, sapply(df, is.integer)] <- Map(as.numeric, df[, sapply(df, is.integer)])
+  names(df) <- ids
+  df
+}
+
+#' Draw the network
+#'
+#' Uses visNetwork to draw a network
+#' @param df Preprocessed dataframe (i.e. discretized numerics and factors)
+#' @param algo Name of algorithm to be used for structure learning
+#' @importFrom dplyr %>%
+#' @import visNetwork
+#' @export
+draw_network <- function(df, algo) {
+  df <- preprocess(df)
+  net <- do.call(algo, list(df))
+
+  nodelist <- data.frame(id = nodes(net),
+                         label = nodes(net))
+  edges <- data.frame(arcs(net))
+  edges[] <- Map(as.character, Filter(is.factor, edges))
+  edges$label <- apply(edges, 1,
+                       function(x) {
+                         round(mutinformation(df[x[1]], df[x[2]]), 2)
+                       })
+
+  visNetwork(nodes = nodelist, edges = edges) %>%
+    visEdges(
+      arrows = list(to = list(enabled = TRUE)),
+      color = list(color = "lightblue", highlight = "red")
+    ) %>%
+    visOptions(
+      nodesIdSelection = TRUE,
+      #manipulation = TRUE,
+      highlightNearest = list(enabled = TRUE, degree = 2)
+    )
+}
+#' Make App
+#'
+#' Make shiny page to create create bayesian networks
+#' @param df The data.frame passed in. No defaults.
+#' @export
+#' @importFrom dplyr %>%
+#' @import bnlearn visNetwork shiny
+#' @examples
+#' bnet(df)
+bnet <- function(df) {
+
+  df <- preprocess(df)
+
+  shinyApp(
+    ### UI SECTION ###
+    ui = fluidPage(
+      titlePanel('bnlearn'),
+      sidebarPanel(
+        selectInput(
+          'algo',
+          label = 'Algorithm',
+          choices = c('iamb', 'hc', 'gs', 'inter.iamb', 'fast.iamb'),
+          selected = 'iamb'
+        )
+      ),
+      mainPanel(visNetworkOutput('network'))
+    ),
+
+    ### SERVER SECTION ###
+    server = function(input, output) {
+      output$network = renderVisNetwork({
+        draw_network(df, input$algo)
+      })
+    }
+  )
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(bnets)
+
+test_check("bnets")
